05d93cc06025b912299cd5e6d39c6635dae0f451
Avirup <avirup.pal@gmail.com>
2019-02-02 21:59:43 +0530

    package names changes

--- src/main/java/com/orastays/configserver/ConfigServerApplication.java
+++ src/main/java/com/orastays/configserver/ConfigServerApplication.java
@@ -1,4 +1,4 @@
-package com.stays.configserver;
+package com.orastays.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
